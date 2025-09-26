cc695f31c12f29a44ae731528d05fd2cfef15584
Lena Merkli <lena@merkli.me>
2025-09-26 16:22:51 +0200

    Configure Angular

--- web/src/app/app.routes.ts
+++ web/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {Create} from './page/account/create/create';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '/account/create', component: Create}
+];
