08577920255e31b300a55ebf1f32011c23ff1a44
Lena Merkli <lena@merkli.me>
2025-10-04 16:07:45 +0200

    Change top bar

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LanguageSwitcher } from './shared/language-switcher/language-switcher';
+import { MatToolbar } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LanguageSwitcher],
+  imports: [RouterOutlet, LanguageSwitcher, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
