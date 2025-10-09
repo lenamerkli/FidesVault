9b2b13fe5cf8fd49302d6cf5d76883d45135b824
Lena Merkli <lena@merkli.me>
2025-10-09 15:43:29 +0200

    Fix encoding bug

--- web/src/app/utils/base64.ts
+++ web/src/app/utils/base64.ts
@@ -25,10 +25,31 @@ export function uint8ArrayToBase64url(bytes: Uint8Array): string {
     return base64;
 }
 
-export function base64_decode(input: string): string {
-    return atob(input);
+const encode_regex = /[\+=\/]/g;
+const decode_regex = /[\._\-]/g;
+
+export function base64_encode(text: string) {
+  return btoa(text).replace(encode_regex, encodeChar);
 }
 
-export function base64_encode(input: string): string {
-    return btoa(input);
+export function base64_decode(text: string) {
+  return atob(text.replace(decode_regex, decodeChar));
+}
+
+function encodeChar(c: string) {
+  switch (c) {
+    case '+': return '.';
+    case '=': return '-';
+    case '/': return '_';
+  }
+  return c;
+}
+
+function decodeChar(c: string) {
+  switch (c) {
+    case '.': return '+';
+    case '-': return '=';
+    case '_': return '/';
+  }
+  return c;
 }
